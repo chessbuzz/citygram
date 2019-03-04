97378a155ea0dfbbf272dc211e920aa9ea7351f6
chessbuzz <chessbuzz@gmail.com>
2019-03-04 15:53:25 -0500

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
-# config/unicorn.rb
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+worker_processes Integer(ENV['WEB_CONCURRENCY'] || 2)
 timeout 15
 preload_app true
+listen(ENV['PORT'] || 3000, :backlog => Integer(ENV['UNICORN_BACKLOG'] || 200))
 
 before_fork do |server, worker|
   Signal.trap 'TERM' do
@@ -9,15 +9,13 @@ before_fork do |server, worker|
     Process.kill 'QUIT', Process.pid
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+  if defined?(Sequel::Model)
+    Sequel::DATABASES.each { |db| db.disconnect }
+  end
 end
 
 after_fork do |server, worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
   end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
 end
